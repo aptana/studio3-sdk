773b4603c74efc803ac041f9f2ecf56d736b8e98
Shalom Gibly <sgibly@aptana.com>
2011-05-31 11:48:02 -0700

    Add a sample for opening a Project Wizard.

--- portal/sample/resources/commands.js
+++ portal/sample/resources/commands.js
@@ -10,7 +10,8 @@ Commands = Class.create({
         	  	tbody(
         	  		tr(td(elm1 = a({'href' : '#'}, "Command #1 - Open the About dialog"))),
         	  		tr(td(elm2 = a({'href' : '#'}, "Command #2 - Open the Search dialog"))),
-        	  		tr(td(elm3 = a({'href' : '#'}, "Command #3 - Command With Parameters - Open the BreakPoints View")))
+        	  		tr(td(elm3 = a({'href' : '#'}, "Command #3 - Command With Parameters - Open the BreakPoints View"))),
+        	  		tr(td(elm4 = a({'href' : '#'}, "Command #4 - Open a 'New Web Project' wizard")))
         	  ));
             viewExampleDiv.appendChild(commands);
             // Observe and report selection changes for this item
@@ -49,6 +50,23 @@ Commands = Class.create({
                         controller : 'portal.commands',
                         action : "execute",
                         args : ["org.eclipse.ui.views.showView", {"org.eclipse.ui.views.showView.viewId" : "org.eclipse.debug.ui.BreakpointView"}].toJSON()
+                    }).toJSON());
+                }
+                return false;
+            });
+            // Observe and report selection changes for this item
+            // NOTES: 
+            // -- This one is a command with parameters
+            // -- "com.appcelerator.titanium.desktop.project_wizard" - Titanium Studio Desktop project wizard ID
+            // -- "com.appcelerator.titanium.mobile.project_wizard" - Titanium Studio Mobile project wizard ID
+            elm4.observe('click', function(e) {
+                inputElement = e.element();
+                if (typeof(console) !== 'undefined' && typeof(dispatch) !== 'undefined') {
+                    console.log("Dispatching the 'execute' action (with parameters) on the 'portal.commands' controller...");
+                    dispatch($H({
+                        controller : 'portal.commands',
+                        action : "execute",
+                        args : ["org.eclipse.ui.newWizard", {"newWizardId" : "com.aptana.ui.wizards.NewWebProject"}].toJSON()
                     }).toJSON());
                 }
                 return false;
