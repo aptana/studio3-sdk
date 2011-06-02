e7587f4a81aedc0b420ce3012ab5daa301203adc
Shalom Gibly <sgibly@aptana.com>
2011-06-01 19:22:11 -0700

    Add an example for opening the 'Samples' view

--- portal/sample/resources/views.js
+++ portal/sample/resources/views.js
@@ -6,10 +6,14 @@ Views = Class.create({
         // Get the div for the Open-View example
         viewExampleDiv = $('openViewId');
         with (Elements.Builder) {
-            elm = a({'href' : '#'}, "Open Bookmarks View");
-            viewExampleDiv.appendChild(elm);
-            // Observe and report selection changes for this item
-            elm.observe('click', function(e) {
+            bookmarksLink = a({'href' : '#'}, "Open Bookmarks View");
+            samplesLink = a({'href' : '#'}, "Open Samples View");
+            viewExampleDiv.appendChild(bookmarksLink);
+            viewExampleDiv.appendChild(br());
+            viewExampleDiv.appendChild(samplesLink);
+            
+            // Observe and report selection changes for the bookmarks-view item
+            bookmarksLink.observe('click', function(e) {
                 inputElement = e.element();
                 if (typeof(console) !== 'undefined' && typeof(dispatch) !== 'undefined') {
                     console.log("Dispatching the 'openView' action on the 'portal.views' controller...");
@@ -17,6 +21,19 @@ Views = Class.create({
                         controller : 'portal.views',
                         action : "openView",
                         args : ["org.eclipse.ui.views.BookmarkView"].toJSON()
+                    }).toJSON());
+                }
+                return false;
+            });
+            // Observe and report selection changes for the samples-view item
+            samplesLink.observe('click', function(e) {
+                inputElement = e.element();
+                if (typeof(console) !== 'undefined' && typeof(dispatch) !== 'undefined') {
+                    console.log("Dispatching the 'openView' action on the 'portal.views' controller...");
+                    dispatch($H({
+                        controller : 'portal.views',
+                        action : "openView",
+                        args : ["com.aptana.samples.ui.SamplesView"].toJSON()
                     }).toJSON());
                 }
                 return false;
